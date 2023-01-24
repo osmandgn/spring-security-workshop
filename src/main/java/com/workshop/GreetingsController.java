38f600ec57ec193aea4a9c4936036812512190b2
osmandgn <oskan001@gmail.com>
2023-01-24 17:05:17 +0300

    V1

---
+++ src/main/java/com/workshop/GreetingsController.java
@@ -0,0 +1,22 @@
+package com.workshop;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GreetingsController {
+
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping("/bye")
+    public ResponseEntity<String> goodBye(){
+        return ResponseEntity.ok("Good Bye");
+    }
+}
